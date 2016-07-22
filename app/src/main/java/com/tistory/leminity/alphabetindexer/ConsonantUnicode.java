ca96a11da05c4dce5c5beaaefd51e4a4c65bd6d3
leminity@gmail.com <zmfltkdhfm103>
2016-07-22 11:05:34 +0900

    # implements  - add api isContainUnicode(check is consonant contain unicode scpoe).

--- app/src/main/java/com/tistory/leminity/alphabetindexer/ConsonantUnicode.java
+++ app/src/main/java/com/tistory/leminity/alphabetindexer/ConsonantUnicode.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class ConsonantUnicode {
 
     private String consonant;
-    private char[] unicodeArray;
+    private char[] unicodeArray; //required define unicode even number
 
     public ConsonantUnicode(String consonant, char[] unicodeArray) {
         this.consonant = consonant;
@@ -21,6 +21,37 @@ public class ConsonantUnicode {
 
     public char[] getUnicodeArray() {
         return unicodeArray;
+    }
+
+    public boolean isContainUnicode(String args) {
+        char consonantArgs = getConsonantChar(args);
+
+        for (int i = 0; i < (unicodeArray.length / 2); i++) {
+            if((unicodeArray[i] >= consonantArgs)
+                    && (unicodeArray[i+1] <= consonantArgs)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static char getConsonantChar(String args) {
+        char consonant;
+        char comVal = (char) (args.trim().charAt(0)-0xAC00);
+
+        if (comVal >= 0 && comVal <= 11172){ // kr
+            char uniVal = (char)comVal; // ignore if inputed only initial consonant.
+
+            char cho = (char) ((((uniVal - (uniVal % 28)) / 28) / 21) + 0x1100); // divide initial consonant depends unicode
+            consonant = cho;
+
+        } else { // if not kr
+            comVal = (char) (comVal+0xAC00);
+            consonant = comVal;
+        }
+
+        return consonant;
     }
 
     @Override
